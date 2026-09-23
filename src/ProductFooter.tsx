03f8ee14147fade4959a0d08b9dcd0a76ff916a5
Eyyüp İsa Karakaşlı <eyyupisa16@gmail.com>
2026-09-23 16:13:22 +0300

    refactor: extract shared product footer

---
+++ src/ProductFooter.tsx
@@ -0,0 +1,12 @@
+export function ProductFooter() {
+  return (
+    <footer className="product-footer">
+      <a href="/privacy/">Privacy</a>
+      <a href="/limitations/">Limitations</a>
+      <a href="mailto:dedupesafe@outlook.com?subject=DedupeSafe%20feedback&amp;body=Role%20%28consultant%2Fagency%20or%20own%20HubSpot%20account%29%3A%0ADid%20you%20try%20an%20authorized%20real%20export%20or%20the%20demo%3F%0AWhat%20worked%2C%20matched%20incorrectly%2C%20or%20was%20missed%3F%0AWhere%20did%20you%20get%20stuck%3F%0AWhere%20did%20you%20find%20DedupeSafe%3F%0A%0APlease%20do%20not%20attach%20contact%20CSVs%2C%20audit%20reports%2C%20or%20customer%20personal%20data.%20Use%20invented%20examples%20if%20needed.">Email feedback: dedupesafe@outlook.com</a>
+      <a href="https://github.com/Eyyupisakarakasli/dedupesafe/issues">Public bug tracker</a>
+      <p>Tell us what worked or went wrong. Do not send contact CSVs, audit reports or customer personal data.</p>
+      <p>Independent of and not authorized, endorsed, sponsored or approved by HubSpot, Inc.</p>
+    </footer>
+  )
+}
